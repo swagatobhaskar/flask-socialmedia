e099f2df07d89e91263cb1f3ac10835010edd7c5
swagatobhaskar <bhaskarswagato@gmail.com>
2024-06-11 23:41:09 +0530

    Added and displayed images from home.static as well as root static folder

--- socialnetwork/app/home/routes.py
+++ socialnetwork/app/home/routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
-home_bp = Blueprint('home', __name__, template_folder='templates', static_folder='static')
+home_bp = Blueprint('home', __name__, template_folder='templates', static_folder='static', static_url_path='/home/static/')
 
 @home_bp.route('/')
 @home_bp.route('/home')
